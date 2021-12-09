f84e214fc55e77846e80f7fb9b096dd465a4846a
HaoChuh <coldy@outlook.com>
2021-12-08 20:02:50 -0800

    add end point path

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from utils.grammar_parser import GrammarParser
 from utils.orchestrator import Orchestrator
 
 
-def main(grammar_file_path):
+def main(grammar_file_path, end_point_path):
     grammar_parser = GrammarParser(grammar_file_path)
     graph = grammar_parser.generate_dependency_graph()
     datatypes = grammar_parser.get_datatypes()
@@ -15,20 +15,21 @@ def main(grammar_file_path):
     # TODO: remove me!
     print(datatypes)
 
-    orchestrator = Orchestrator(graph)
+    orchestrator = Orchestrator(graph, end_point_path)
     orchestrator.orchestrate()
 
 
 if __name__ == "__main__":
-    number_of_arguments = 1
+    number_of_arguments = 2
 
     if len(sys.argv) < number_of_arguments + 1:
         print(f"(+) Requires {number_of_arguments} arguments")
-        print(f"(+) Example usage: python3 main.py examples/grammar-example.yml")
+        print(f"(+) Example usage: python3 main.py examples/grammar-example.yml http://localhost:1234")
         sys.exit()
 
     file_name = sys.argv[0]
     grammar_file_path = sys.argv[1]
+    end_point_path = sys.argv[2]
 
     print("(+) Starting Graphler program")
     main(grammar_file_path)
--- utils/orchestrator.py
+++ utils/orchestrator.py
@@ -6,10 +6,12 @@ James
 
 class Orchestrator:
     graph = None
+    end_point_path = None
 
     # constructor
-    def __init__(self, graph):
+    def __init__(self, graph, end_point_path):
         self.graph = graph
+        self.end_point_path = end_point_path
 
     # main orchestrate function
     def orchestrate(self):
