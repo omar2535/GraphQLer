c9266363d48dbe46d269a079954197c236a6097f
Omar2535 <omar2535@alumni.ubc.ca>
2023-09-29 00:10:30 -0400

    Add object type parsing of introspection query result

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -4,14 +4,16 @@
 from pathlib import Path
 from compiler.utils import send_graphql_request, write_json_to_file
 from compiler.introspection_query import introspection_query
+from compiler.parsers.object_list_parser import ObjectListParser
 
 import constants
+import yaml
 
 
 class Compiler:
     def __init__(self, save_path: str, url: str):
         """Initializes the compiler,
-            creates all necessary file paths for run if doesn't already exist
+            creates all necessary file paths to save the outputs for run if doesn't already exist
 
         Args:
             save_path (str): Save directory path
@@ -20,6 +22,7 @@ class Compiler:
         self.save_path = save_path
         self.introspection_result_save_path = Path(save_path) / constants.INTROSPECTION_RESULT_FILE_NAME
         self.function_list_save_path = Path(save_path) / constants.FUNCTION_LIST_FILE_NAME
+        self.object_list_save_path = Path(save_path) / constants.OBJECT_LIST_FILE_NAME
         self.mutation_parameter_save_path = Path(save_path) / constants.MUTATION_PARAMETER_FILE_NAME
         self.query_parameter_save_path = Path(save_path) / constants.QUERY_PARAMETER_FILE_NAME
         self.schema_save_path = Path(save_path) / constants.SCHEMA_FILE_NAME
@@ -28,6 +31,7 @@ class Compiler:
         Path(self.save_path).mkdir(parents=True, exist_ok=True)
         open(self.introspection_result_save_path, "a").close()
         open(self.function_list_save_path, "a").close()
+        open(self.object_list_save_path, "a").close()
         open(self.mutation_parameter_save_path, "a").close()
         open(self.query_parameter_save_path, "a").close()
         open(self.schema_save_path, "a").close()
@@ -38,9 +42,27 @@ class Compiler:
         2. Parsing through results
         3. Storing files into query / mutations
         """
-        self.get_introspection_query()
+        introspection_result = self.get_introspection_query()
+        self.parse_and_save_object_list(introspection_result)
 
-    def get_introspection_query(self):
-        """Run the introspection query, grab results and output to file"""
+    def get_introspection_query(self) -> dict:
+        """Run the introspection query, grab results and output to file
+
+        Returns:
+            dict: Dictionary of the resulting JSON from the introspection query
+        """
         result = send_graphql_request(self.url, introspection_query)
         write_json_to_file(result, self.introspection_result_save_path)
+        return result
+
+    def parse_and_save_object_list(self, introspection_result: dict):
+        """Parse and save the object list from the introspection query result dictionary
+
+        Args:
+            introspection_result (dict): Introspection query result as a dictionary
+        """
+        object_list_parser_instance = ObjectListParser()
+        parsed_object_list = object_list_parser_instance.parse(introspection_result)
+        yaml_data = yaml.dump(parsed_object_list, default_flow_style=False)
+        with open(self.object_list_save_path, "w") as yaml_file:
+            yaml_file.write(yaml_data)
---
+++ compiler/parsers/object_list_parser.py
@@ -0,0 +1,56 @@
+"""Simple singleton class to parse object listings from the introspection query"""
+from typing import List
+
+
+class ObjectListParser:
+    def __init__(self):
+        self.excluded_types = [
+            "Mutation",
+            "Query",
+            "__Schema",
+            "__Type",
+            "__TypeKind",
+            "__Field",
+            "__InputValue",
+            "__EnumValue",
+            "__Directive",
+            "__DirectiveLocation",
+        ]
+
+    def __extract_field_info(self, field):
+        field_info = {
+            "name": field["name"],
+            "kind": field["type"]["kind"],
+            "type": field["type"]["name"] if "name" in field["type"] else None,
+            "ofType": field["type"]["ofType"]["name"]
+            if "ofType" in field["type"] and field["type"]["ofType"] and "name" in field["type"]["ofType"]
+            else None,
+        }
+        return field_info
+
+    def parse(self, introspection_data: dict) -> List[dict]:
+        """Parses the introspection data for only objects
+
+        Args:
+            data (dict): Introspection JSON as a dictionary
+
+        Returns:
+            List[dict]: List of objects with their types
+        """
+        # Grab just the objects from the dict
+        schema_types = introspection_data.get("data", {}).get("__schema", {}).get("types", [])
+        object_types = [
+            t for t in schema_types if t.get("kind") == "OBJECT" and t.get("name") not in self.excluded_types
+        ]
+
+        # Convert it to the YAML structure we want
+        object_info_dict = {}
+        for obj in object_types:
+            object_name = obj["name"]
+            object_info_dict[object_name] = {
+                "kind": obj["kind"],
+                "name": object_name,
+                "fields": [self.__extract_field_info(field) for field in obj["fields"]],
+            }
+
+        return object_info_dict
