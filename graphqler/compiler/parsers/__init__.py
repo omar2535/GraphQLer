3dc9109d8fa40372eb04711329b988f069738eac
Omar2535 <omar2535@alumni.ubc.ca>
2024-07-14 13:34:25 -0400

    Add support for UNION types

--- graphqler/compiler/parsers/__init__.py
+++ graphqler/compiler/parsers/__init__.py
@@ -7,4 +7,5 @@ from .query_list_parser import QueryListParser
 from .mutation_list_parser import MutationListParser
 from .input_object_list_parser import InputObjectListParser
 from .enum_list_parser import EnumListParser
+from .union_list_parser import UnionListParser
 from .parser import Parser
---
+++ graphqler/compiler/parsers/union_list_parser.py
@@ -0,0 +1,55 @@
+"""
+Enum objects: https://spec.graphql.org/October2021/#sec-Unions
+"""
+from .parser import Parser
+from typing import List
+
+
+class UnionListParser(Parser):
+    def __init__(self):
+        self.excluded_types = [
+            "Mutation",
+            "Query",
+            "__Schema",
+            "__Type",
+            "__TypeKind",
+            "__Field",
+            "__InputValue",
+            "__EnumValue",
+            "__Directive",
+            "__DirectiveLocation",
+        ]
+
+    def extract_union_values(self, union_values: List[dict]) -> List[dict]:
+        """Extract only the relavent fields from the possible union values
+
+        Args:
+            union_valies (List[dict]): List of possible values of the UNION object
+
+        Returns:
+            List[dict]: List of possible balues of the UNION object but filtered for only relavent fields
+        """
+        list_of_union_values = []
+        for union_value in union_values:
+            filtered_union_value = {"kind": union_value["kind"], "name": union_value["name"], "ofType": union_value["ofType"]}
+            list_of_union_values.append(filtered_union_value)
+        return list_of_union_values
+
+    def parse(self, introspection_data: dict) -> dict:
+        """Parses the introspection data for only objects
+
+        Args:
+            data (dict): Introspection JSON as a dictionary
+
+        Returns:
+            dict: List of objects with their types
+        """
+        # Grab just the objects from the dict
+        schema_types = introspection_data.get("data", {}).get("__schema", {}).get("types", [])
+        union_objects = [t for t in schema_types if t.get("kind") == "UNION" and t.get("name") not in self.excluded_types]
+
+        built_union_objects = {}
+        for union_object in union_objects:
+            union_name = union_object["name"]
+            built_union_objects[union_name] = {"possibleTypes": self.extract_union_values(union_object["possibleTypes"])}
+        return built_union_objects
