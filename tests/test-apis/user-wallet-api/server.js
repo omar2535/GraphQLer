c88d56def5ae73718c9e0ee4b620ca796548aa99
Omar2535 <omar2535@alumni.ubc.ca>
2024-08-08 00:58:55 -0700

    Fix integration tests

--- tests/test-apis/user-wallet-api/server.js
+++ tests/test-apis/user-wallet-api/server.js
@@ -1,26 +1,40 @@
 import express from 'express';
-import {ApolloServer} from 'apollo-server-express';
-import {createServer} from 'http';
+import { ApolloServer } from 'apollo-server-express';
+import { createServer } from 'http';
 import cors from 'cors';
+import { useServer } from 'graphql-ws/lib/use/ws';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 import schema from './data/schema.js';
+import { WebSocketServer } from 'ws';
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use('*', cors());
 
-const server = new ApolloServer({
-    schema,
-    playground: true,
-})
-
-server.applyMiddleware({app});
-
 const httpServer = createServer(app);
 
-server.installSubscriptionHandlers(httpServer);
+const startApolloServer = async () => {
+  const server = new ApolloServer({
+    schema,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
 
-httpServer.listen(PORT, () => {
-  console.log(`> Server ready at http://localhost:${PORT}${server.graphqlPath}`);
-  console.log(`> Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`);
-});
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: server.graphqlPath,
+  });
+
+  useServer({ schema }, wsServer);
+
+  httpServer.listen(PORT, () => {
+    console.log(`> Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`> Subscriptions ready at ws://localhost:${PORT}${server.graphqlPath}`);
+  });
+};
+
+startApolloServer();
