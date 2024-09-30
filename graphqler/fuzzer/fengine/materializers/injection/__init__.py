bf19d71e733187d84b057da170b05ef32bebd32f
Omar2535 <omar2535@alumni.ubc.ca>
2024-09-30 15:24:15 -0400

    Add HTML Injection materializer

--- graphqler/fuzzer/fengine/materializers/injection/__init__.py
+++ graphqler/fuzzer/fengine/materializers/injection/__init__.py
@@ -1,5 +1,7 @@
 from .sql_injection_materializer import SQLInjectionMaterializer
+from .html_injection_materializer import HTMLInjectionMaterializer
 
 injection_materializers = [
-    SQLInjectionMaterializer
+    SQLInjectionMaterializer,
+    HTMLInjectionMaterializer
 ]
