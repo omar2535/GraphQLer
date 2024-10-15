d4e26bc5117346475f88a679d24506619ea632e2
Omar2535 <omar2535@alumni.ubc.ca>
2024-10-14 22:02:59 -0400

    Add objects bucket refactoring

--- graphqler/utils/objects_bucket.py
+++ graphqler/utils/objects_bucket.py
@@ -1,16 +1,34 @@
 """Class for an objects bucket to contain the history of all objects in the system under test
+What does an objects bucket track?
+- For each type of object, track any values that were associated to that object to be used later
+- For each kind of scalar, track any values seen to be used later
+
+TODO: Implement the following:
+The class should have two functionalities
+1. Given the graphql data response, parse the data and put objects in the bucket
+2. Be able to return random scalars / objects from the bucket
+3. Be able to return objects from the bucket if given a type and the object name
 """
 
 from graphqler.constants import USE_OBJECTS_BUCKET
 from .singleton import singleton
+from graphqler.utils.api import API
+from graphqler.utils.parser_utils import get_output_type_from_details
 import random
 import pprint
 
 
 @singleton
 class ObjectsBucket:
-    def __init__(self):
+    def __init__(self, api: API):
         self.bucket = {}
+        self.api = api
+
+        # Stores {object_name: set() }} where set() is a result with the scalar fields of the object
+        self.objects: dict[str, set] = {}
+
+        # Stores the raw scalars {scalar_name: {type: str, values: set() }} where set() is a result with the scalar fields of the object
+        self.scalars: dict[str, dict] = {}
 
     def __str__(self):
         return pprint.pformat(self.bucket)
@@ -26,78 +44,71 @@ class ObjectsBucket:
         """
         return name in self.bucket
 
-    def put_in_bucket(self, name: str, val: str) -> dict:
+    def put_in_bucket(self, response_data: dict) -> bool:
         """Puts an object in the bucket, returns the new bucket. If the object already exists, it will not be added.
 
         Args:
-            name (str): The objects name
-            val (str): The objects value (for example ID)
+            response_data (dict): The data to put in the bucket. This is the responses data from GraphQL
 
         Returns:
-            dict: The new bucket with the object_name: [..., object_val]
+            bool: True if the object was added, False otherwise
         """
         # If we're not using the objects bucket, just return an empty dict
         if not USE_OBJECTS_BUCKET:
-            return {}
+            return False
 
-        if name in self.bucket:
-            self.bucket[name].add(val)
+        # If no data, just return
+        if not response_data:
+            return False
+
+        # Iterate through the data, put in the bucket
+        for data_key, data in response_data.items():
+            if self.api.is_operation_in_api(data_key):
+                self.parse_as_object(data_key, data)
+
+            else:
+                # This means the operation name isn't known to us -- just parse the data as a regular scalar
+                self.parse_as_scalar(data_key, data)
+        return True
+
+    def parse_as_object(self, operation_name: str, data: dict | list[dict]):
+        """Parses the data as an object by looking up the output of the operation in the API
+
+        Args:
+            operation_name (str): The operation name, should be an operation in the API
+            data (dict | List[dict]): The data to parse
+        """
+        # Get the operation from the API
+        operation = self.api.get_operation(operation_name)
+        operation_output_type = get_output_type_from_details(operation)
+
+        if isinstance(data, list):
+            for item in data:
+                self.put_object_in_bucket(operation_output_type, item)
         else:
-            self.bucket[name] = set([val])
-        return self.bucket
+            self.put_object_in_bucket(operation_output_type, data)
 
-    def remove_from_object_bucket(self, name: str, val: str) -> dict:
-        """Removes an object in the bucket, returns the new bucket. If the object doesn't exist, it will not be removed.
+    def parse_as_scalar(self, method_name: str, method_data: str):
+        """Parses the data as a scalar
 
         Args:
-            name (str): The objects name
-            val (str): The objects value
-
-        Returns:
-            dict: The new bucket
+            method_name (str): The method name
+            method_data (str): The method data
         """
-        # If we're not using the objects bucket, just return an empty dict
-        if not USE_OBJECTS_BUCKET:
-            return {}
+        if isinstance(method_data, str):
+            self.put_scalar_in_bucket(method_name, "String", method_data)
 
-        if name in self.bucket:
-            if val in self.bucket[name]:
-                self.bucket[name].remove(val)
-        return self.bucket
+    def put_object_in_bucket(self, object_name: str, object_info: dict):
+        pass
 
-    def get_closest_key_to_bucket(self, input_name: str, objects_bucket: dict) -> str:
-        """Tries to find the object name if it has ID behind, if not, then chooses at random
+    def put_scalar_in_bucket(self, name: str, type: str, data: str):
+        """Puts scalar in the bucket
+
         Args:
-            input_name (str): The input name
-            objects_bucket (dict): The objects bucket
-
-        Returns:
-            str: The key of the object
+            name (str): The scalar's name
+            type (str): The scalar's type
+            data (str): The scalar's data
         """
-        # Tries for the object name
-        if input_name.endswith("Id"):
-            object_name = input_name[0].capitalize() + input_name[1:-2]
-            if object_name in objects_bucket:
-                return object_name
-
-        # Gives up and gets a key
-        return random.choice(list(objects_bucket.keys()))
-
-    def get_random_id_from_bucket(self, input_name: str, objects_bucket: dict) -> str:
-        """Tries to get an ID from the bucket based on the input_name first, then just randomly chooses an ID from the bucket,
-           if the bucket is empty, then just returns an empty string
-        Gets a random ID from the bucket, or just "" if there are no IDs in the bucket
-        Args:
-            input_name (str): The input name
-            objects_bucket (dict): Object bucket
-
-        Returns:
-            str: an ID
-        """
-        # If it's empty, just return a random ID
-        if not objects_bucket:
-            return ""
-
-        key = self.get_closest_key_to_bucket(input_name, objects_bucket)
-        random_object = random.choice(objects_bucket[key])
-        return f'"{random_object}"'
+        if name not in self.scalars:
+            self.scalars[name] = {"type": type, "values": set(data)}
+        self.scalars[name]["values"].add(data)
