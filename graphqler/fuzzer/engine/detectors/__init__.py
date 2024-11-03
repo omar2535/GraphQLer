dc24d35b1d1195aef4e093a46a20cc044a883071
Omar2535 <omar2535@alumni.ubc.ca>
2024-11-03 18:43:10 -0500

    Fix failed introspection -> clairvoyance error & add SQL Injection detector

--- graphqler/fuzzer/engine/detectors/__init__.py
+++ graphqler/fuzzer/engine/detectors/__init__.py
@@ -1,6 +1,7 @@
 from .os_command_injection.os_command_injection_detector import OSCommandInjectionDetector
 from .xss_injection.xss_injection_detector import XSSInjectionDetector
 from .ssrf_injection.ssrf_injection_detector import SSRFInjectionDetector
+from .sql_injection.sql_injection_detector import SQLInjectionDetector
 
 from .introspection.introspection_detector import IntrospectionDetector
 from .field_suggestion.field_suggestion_detector import FieldSuggestionsDetector
@@ -9,7 +10,8 @@ from .field_suggestion.field_suggestion_detector import FieldSuggestionsDetector
 injection_detectors = [
     OSCommandInjectionDetector,
     XSSInjectionDetector,
-    SSRFInjectionDetector
+    SSRFInjectionDetector,
+    SQLInjectionDetector
 ]
 
 api_detectors = [
